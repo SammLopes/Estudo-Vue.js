93f5cee2a1288cac92b94e895932d5e178038653
Samuel Venturini Paviotti Lopes <ti@meupatrimonio.com>
2023-07-31 16:24:01 -0300

    Alterações feitas porem incompletas usadas o JavaScript Vanilla

--- contador/index.js
+++ contador/index.js
@@ -3,6 +3,27 @@ const btnAdd = document.getElementById('add');
 const btnRem = document.getElementById('reduce');
 const resultado = document.getElementById('res');
 const inter = document.getElementById('inter');
+const entrada =  document.getElementById('entrada');
+const saida = document.getElementById('saida');
+const form = document.getElementById('form');
+const dadosEntrada = document.getElementById('formEntrada');
+const btnEnviar = document.getElementById('enviar');
+const intEnt = document.getElementById('ent');
+const intOut = document.getElementById('out');
+
+
+function intCampo1(){
+    intEnt.textContent = inter.value;
+    intOut.textContent = inter.value;
+}
+
+function intCampo2(){
+    inter.textContent = intEnt.value;
+}
+
+function intCampo3(){
+    intOut.textContent = inter.value;
+}
 
 function updateDisplay(){
     resultado.textContent = count;
@@ -26,12 +47,6 @@ function reduce(event){
     }
 }
 
-btnAdd.addEventListener('click', add);
-btnRem.addEventListener('click', reduce);
-
-const entrada =  document.getElementById('entrada');
-const saida = document.getElementById('saida');
-
 function atualizaCampo(){
     saida.textContent = entrada.value;
 }
@@ -40,5 +55,19 @@ function atualizaCampo2(){
     entrada.textContent = saida.value;
 }
 
+function enviarForms(event){
+    event.preventDefault();
+    alert('Seus dados foram enviados');
+}
+
+
+
+
+btnEnviar.addEventListener('click', enviarForms);
+btnAdd.addEventListener('click', add);
+btnRem.addEventListener('click', reduce);
 entrada.addEventListener('input', atualizaCampo);
 saida.addEventListener('input', atualizaCampo2);
+intEnt.addEventListener('input', intCampo2);
+inter.addEventListener('input', intCampo1);
+intOut.addEventListener('input', intCampo3);
