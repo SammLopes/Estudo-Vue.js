5bf3cdc83e01f8525eb5e0afaf71eea3cb906c07
Samuel Venturini Paviotti Lopes <ti@meupatrimonio.com>
2023-07-31 15:10:00 -0300

    Projeto atualizado

--- contador-vue/app.js
+++ contador-vue/app.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
             counter: 0,
             inter: 0,
             name: '',
+            confirmarName: '',
         }
     },
 
@@ -28,6 +29,21 @@ const app = Vue.createApp({
                 this.name = event.target.value + " " + lastName;
             }    
         },
+
+        setInter(event, num){
+            if(!isNaN(num)){
+                this.num = event.target.value;
+            }
+        },
+
+        envioForm(){
+            //event.preventDefault() usado apra imperdir o reload da pagina logo o envio do formulário
+            alert('Dados enviados com sucesso!!!');
+        },
+        
+        confirmaNome(){
+            this.confirmarName = this.name;
+        }
     }
 });
 app.mount('#events');
